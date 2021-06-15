27d72ecd1ad59221bf5599819f1419d6ed769022
Naveen-dev-123 <85943916+Naveen-dev-123@users.noreply.github.com>
2021-06-15 19:34:16 +0530

    Add files via upload
    This project generates random Cat gif when click the generate button.

---
+++ Cat Generator/catGenerator.js
@@ -0,0 +1,7 @@
+function generateCat(){
+    let image = document.createElement('img');
+    let div = document.getElementById('image-box')
+    image.src = "https://media3.giphy.com/media/tRoH9EYLs3lok/giphy.gif";
+    div.appendChild(image);
+    
+}
